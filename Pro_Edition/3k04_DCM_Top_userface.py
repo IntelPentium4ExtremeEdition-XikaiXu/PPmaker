67b9f1b82a714d93d560fa476dd010e7d39660c4
Xikai Xu <xuxikai12300@gmail.com>
2024-11-26 17:55:01 -0500

    lol

--- Pro_Edition/3k04_DCM_Top_userface.py
+++ Pro_Edition/3k04_DCM_Top_userface.py
@@ -208,15 +208,12 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("User Login")
     root.geometry("300x200")
-
     tk.Label(root, text="Username:").pack(pady=10)
     entry_username = tk.Entry(root)
     entry_username.pack(pady=5)
-
     tk.Label(root, text="Password:").pack(pady=10)
     entry_password = tk.Entry(root, show="*")
     entry_password.pack(pady=5)
-
     tk.Button(root, text="Login", command=login).pack(pady=10)
     tk.Button(root, text="Register", command=open_register_window).pack(pady=5)
 
